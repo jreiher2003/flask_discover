1720ef30a55648eec8658d20b9c8cb34d4961334
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-10 18:18:08 -0500

    list comp

--- flask_project/app.py
+++ flask_project/app.py
@@ -26,7 +26,7 @@ def login_required(f):
 @login_required
 def home():
     g.db = connect_db()
-    cur = g.db.execute('select * from posts')
+    cur = g.db.execute('select * from posts;')	
     posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
     g.db.close()
     return render_template('index.html', posts=posts)  # render a template

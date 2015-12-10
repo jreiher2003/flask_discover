208db73b16a20b76f18f854fc16aded59d8669d9
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-10 15:06:29 -0500

    add requirement.txt, add message flashing, and categories

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,10 +1,6 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-	return render_template('home.html')
 
 @app.route('/welcome')
 def welcome():
@@ -15,14 +11,27 @@ def login():
 	error = None
 	if request.method == 'POST':
 		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid credentials. Please try again.'
+			flash('Invalid credentials. Please try again.', 'danger')
 		else:
+			session['logged_in'] = True
+			flash('Your logged in!', 'success')
 			return redirect(url_for('home'))
-	return render_template('login.html', error=error)
+	return render_template('login.html')
 
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You just logged out!', 'danger')
+    return redirect(url_for('welcome'))
 
 
 
 if __name__ == '__main__':
+	app.secret_key = 'jeffrey'
 	app.debug = True
 	app.run(host='0.0.0.0', port=5000)

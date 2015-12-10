c3cef940a96ab3ec2a2ffbedca95ec9907a264d3
Jeff Reiher <jreiher2003@yahoo.com>
2015-12-10 16:06:44 -0500

    templates with messge flashing

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,32 +1,56 @@
-from flask import Flask, render_template, redirect, request, url_for, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
+# create the application object
 app = Flask(__name__)
+
+
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
+# use decorators to link the function to a url
+@app.route('/')
+@login_required
+def home():
+    return render_template('index.html')  # render a template
+    # return "Hello, World!"  # return a string
+
 
 @app.route('/welcome')
 def welcome():
-	return render_template('welcome.html')
+    return render_template('welcome.html')  # render a template
 
+
+# route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			flash('Invalid credentials. Please try again.', 'danger')
-		else:
-			session['logged_in'] = True
-			flash('Your logged in!', 'success')
-			return redirect(url_for('home'))
-	return render_template('login.html')
-
-@app.route('/')
-def home():
-	return render_template('home.html')
+    error = None
+    if request.method == 'POST':
+        if (request.form['username'] != 'admin') \
+                or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in.')
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('logged_in', None)
-    flash('You just logged out!', 'danger')
+    flash('You were logged out.')
     return redirect(url_for('welcome'))
 
 
